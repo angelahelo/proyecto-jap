9db7bb42e80a947ce5551ec2cd4ba3ef0afa7fb0
angelahelo <angelahernandezlopez@gmail.com>
2022-10-16 10:02:15 -0300

    pronta entrega 5

--- js/cart.js
+++ js/cart.js
@@ -8,63 +8,69 @@ function showCarUser() {
 
 
     let liCar = `
-    <div class= "container">
-        <h1 class="text-center">Carrito de compras</h1>
+    <div class="container">
+    <h1 class="text-center">Carrito de compras</h1>
 
-        <h2 class="especial">Artículos a comprar</h2>
+    <h2 class="especial">Artículos a comprar</h2>
 
-        <div class= "row" id="table">
-         
+    <div class="row" id="table">
+
         <div class="col">
 
         </div>
-    
-        <div class="col">
-         <p><strong>Nombre</strong></p>
-        </div>
-         <div class="col">
-            <p><strong>Costo</strong></p>
-        </div>
-        
-        <div class="col">
-            <p><strong>Cantidad</strong></p>
-        </div>
-        <div class="col">
-            <p><strong>Subtotal</strong></p>
-        </div>
-        
-    
 
-</div>
-    <div class= "row" id="table2">
-        
         <div class="col">
-           <img src="${car.image}" alt="img-prod"  class="img-thumbnail">
-         </div>
-    
-            <div class="col">
-             <p>${car.name}<p>
-            </div>
-         <div class="col">
+            <p>Nombre</p>
+        </div>
+        <div class="col">
+            <p>Costo</p>
+        </div>
+
+        <div class="col">
+            <p>Cantidad</p>
+        </div>
+        <div class="col-1"></div>
+        <div class="col">
+            <p>Subtotal</p>
+        </div>
+
+
+
+    </div>
+    <div class="row" id="table2">
+
+        <div class="col">
+            <img src="${car.image}" alt="img-prod" class="img-thumbnail">
+        </div>
+
+        <div class="col">
+            <p>${car.name}
+            <p>
+        </div>
+        <div class="col">
             <p>${car.currency}${car.unitCost}</p>
         </div>
-        
+
         <div class="col">
-        
-        <input type="number" name="numero" id="numero"  min= "1" value="1" onchange ="setCantidad(this.value)">
-            
-        </div>
-        <div class="col">
-            <p>${car.currency}</p> <p id= "prueba">${car.unitCost}</p>
-        </div>
-        
+
+            <input type="number" name="numero" id="numero" min="1" value="${car.count}">
+            <span onchange="setCantidad(this.value)"></span>
 
         </div>
-        
+        <div class="col-1"><strong>${car.currency}</strong></div>
+        <div class="col" id="subT">
+
+            <p id="subTotal"> ${car.unitCost}</p>
+
+        </div>
+
+
     </div>
+
+</div>
     `
     console.log(typeof (this.value));
-    //puse en el input value="${car.count}" para que me quede precargado el valor....
+    //puse en el input value= para que me quede precargado el valor....
 
     document.getElementById("carrito").innerHTML += liCar;
 
@@ -79,30 +85,7 @@ function setCantidad(value) {
 
 
 
-/* function showSubTotal (){
-    let subT = `
-    <div>
-    <p>${miSubTotal()}</p>
-    </div>
-    `
-    document.getElementById("total").innerHTML += subT
-} */
 
-
-/* function miSubTotal(value){ // la funcion la prueba en consola y funciona....
-        
-    let precio = carUser.articles[0].unitCost;
-    
-    
-    console.log(precio);
-    console.log(value)
-
-    value * precio;
-    console.log(value * precio);
-
-    
-
-} */
 
 
 
@@ -124,7 +107,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 
-            document.getElementById("prueba").innerHTML = cantidad * precio;
+            document.getElementById("subTotal").innerHTML = cantidad * precio;
         });
         
 
